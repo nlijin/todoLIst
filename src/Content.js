90c59f83e34b8706718bf3b87f8cfdf6d0bb7fe2
Lijin <nlijinraj@gmail.com>
2022-12-07 14:44:47 +0530

    initial git

---
+++ src/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ItemList from "./ItemList";
+
+const Content = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <main>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>There are no items to display</p>
+      )}
+    </main>
+  );
+};
+
+export default Content;
